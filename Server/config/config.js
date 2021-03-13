1dbd364181725b9c752e158e1b0d694553f63077
Cristobal <cristobal.tbecerraf@gmail.com>
2021-03-13 11:07:43 -0300

    fix client endpoints

--- Server/config/config.js
+++ Server/config/config.js
@@ -11,11 +11,8 @@ process.env.TOKEN_SECRET = 'ady7asdy78'
 process.env.MONGO_URI = 'mongodb://localhost:27017/neurone-adv';
 
 //NEURONE GM
-
 process.env.NEURONEGM = 'http://localhost:3080'
 
-//NEURONE GAME CLIENT
-process.env.GAME_CLIENT = 'http://localhost:4200';
 
 //File uploads path
 process.env.UPLOAD_PATH = './images'
